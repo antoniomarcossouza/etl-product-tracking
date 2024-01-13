c02c4a207e2f98da22de61f7a53501f33fc75e6c
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2024-01-13 16:45:59 -0300

    refactor: pasta para o arquivo principal

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 """Script de extração dos dados"""
 
+import ast
+import json
 import os
 import glob
 
 import pandas as pd
-
-# from joblib import Parallel, delayed
 
 
 def process_file(csv_file: str):
@@ -13,22 +13,19 @@ def process_file(csv_file: str):
 
     df = pd.read_csv(csv_file)
 
-    print(csv_file)
-    print(df)
+    print(csv_file.split("/")[-1][:-4])
 
-    # os.remove(csv_file)
-
+    for _, value in df["array_trackingEvents"].items():
+        tracking_events = ast.literal_eval(value)
+        break
 
 if __name__ == "__main__":
     files = glob.glob(os.path.join("./data", "*.csv"))
 
-    # Processa os arquivos de 5 em 5
     for i in range(0, len(files), 5):
         file_list = files[i : i + 5]
 
-        # Parallel(n_jobs=-1)(
-        #     delayed(process_file)(arquivo) for arquivo in file_list
-        # )
-
         for file in file_list:
             process_file(file)
+            break
+        break
