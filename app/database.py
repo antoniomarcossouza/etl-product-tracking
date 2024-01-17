f04fdbcd51ae9f96023da064c5c245d7172fea4c
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2024-01-17 11:55:26 -0300

    feature: query terminada

--- app/database.py
+++ app/database.py
@@ -6,13 +6,13 @@ from datetime import datetime
 import pandas as pd
 
 
-def format_date(date: str):
+def format_date(date: str) -> str:
     """Formata a data para o padrão do banco de dados"""
 
     return datetime.fromtimestamp(date).strftime("%Y-%m-%d %H:%M:%S.%f")
 
 
-def create_values_string(values: list):
+def create_values_string(values: list) -> str:
     """Cria uma string com os valores para a query"""
 
     return ", ".join(
@@ -23,8 +23,34 @@ def create_values_string(values: list):
     )
 
 
+def create_tracking_events_values(fk: str, values: list):
+    """Agrupa os valores em uma string para a query"""
+
+    def format_item(item):
+        """Formata o evento de rastreio para a query"""
+
+        return create_values_string(
+            [
+                fk,
+                item["trackingCode"],
+                format_date(item["createdAt"]["$date"] / 1000),
+                item["status"],
+                item["description"],
+                item["trackerType"],
+                item["from"],
+                item["to"],
+            ]
+        )
+
+    formatted_items = [f"({format_item(item)})" for item in values]
+
+    return ", ".join(formatted_items)
+
+
 def build_query(row: pd.Series):
     """Constrói a query para inserir a linha do DataFrame"""
+    if (ast.literal_eval(row['array_trackingEvents'])) == []:
+        return None
 
     operation_values = [
         row["oid__id"],
@@ -35,7 +61,7 @@ def build_query(row: pd.Series):
 
     query = "BEGIN;\n"
     query += "INSERT INTO st_operations (id, created_at, updated_at, last_sync_tracker)\n"
-    query += f"VALUES ({create_values_string(operation_values)});\n\n"
+    query += f"VALUES ({create_values_string(values=operation_values)});\n\n"
 
     query += "UPDATE t_operations\n"
     query += "SET updated_at = st_operations.updated_at, last_sync_tracker = st_operations.last_sync_tracker\n"
@@ -48,47 +74,21 @@ def build_query(row: pd.Series):
     query += "LEFT JOIN t_operations ON t_operations.id = st_operations.id\n"
     query += "WHERE t_operations.id IS NULL;\n\n"
 
-    query += "DELETE FROM st_operations;\n"
+    query += "DELETE FROM st_operations;\n\n"
 
+    query += "INSERT INTO st_tracking_events (operation_id, tracking_code, created_at, status, description, tracker_type, origin, destination)\n"
+    query += f"VALUES {create_tracking_events_values(fk=row['oid__id'],values=ast.literal_eval(row['array_trackingEvents']))};\n\n"
 
+    query += "UPDATE t_tracking_events\n"
+    query += "SET status = st_tracking_events.status, description = st_tracking_events.description, tracker_type = st_tracking_events.tracker_type, origin = st_tracking_events.origin, destination = st_tracking_events.destination\n"
+    query += "FROM st_tracking_events\n"
+    query += "WHERE t_tracking_events.operation_id = st_tracking_events.operation_id AND t_tracking_events.tracking_code = st_tracking_events.tracking_code AND t_tracking_events.created_at = st_tracking_events.created_at;\n"
 
-
-    # REFATORAR EVENTOS
-
-    # for item in ast.literal_eval(row["array_trackingEvents"]):
-    #     created_at = format_date(item["createdAt"]["$date"] / 1000)
-    #     tracking_code = item["trackingCode"]
-    #     status = item["status"]
-    #     description = item["description"]
-    #     tracker_type = item["trackerType"]
-    #     origin = item["from"]
-    #     destination = item["to"]
-
-    #     tracking_event = create_upsert(
-    #         table_name="tracking_events",
-    #         columns=[
-    #             "operation_id",
-    #             "tracking_code",
-    #             "created_at",
-    #             "status",
-    #             "description",
-    #             "tracker_type",
-    #             "origin",
-    #             "destination",
-    #         ],
-    #         values=[
-    #             op_id,
-    #             tracking_code,
-    #             created_at,
-    #             status,
-    #             description,
-    #             tracker_type,
-    #             origin,
-    #             destination,
-    #         ],
-    #     )
-
-    #     query += f"{chr(9)}{tracking_event}{chr(10)}"
+    query += "INSERT INTO t_tracking_events\n"
+    query += "SELECT st_tracking_events.*\n"
+    query += "FROM st_tracking_events\n"
+    query += "LEFT JOIN t_tracking_events ON t_tracking_events.operation_id = st_tracking_events.operation_id AND t_tracking_events.tracking_code = st_tracking_events.tracking_code\n"
+    query += "WHERE t_tracking_events.operation_id IS NULL AND t_tracking_events.tracking_code IS NULL;\n"
 
     query += "COMMIT;"
     return query
